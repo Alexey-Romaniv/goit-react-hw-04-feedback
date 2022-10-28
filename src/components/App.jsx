edb2ff01294fca398e1ae7bf07adf1318b1f615a
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2022-10-28 19:32:11 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,7 @@ import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 
-export const Feedback = () => {
-  // state = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  // };
+export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -62,5 +57,3 @@ export const Feedback = () => {
     );
   };
 };
-
-export default Feedback;
